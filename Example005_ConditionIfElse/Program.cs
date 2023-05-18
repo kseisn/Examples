81b49c6f08480939e2930bb6104880619c7f61ee
Kami Seisen <kamiseisen@gmail.com>
2023-05-18 18:33:00 +0200

    Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Add your name: ");
+string username = Console.ReadLine();
+if(username.ToLower == "kami")
+    {
+        Console.WriteLine("You are GORGEOUS!");
+    }
+else
+    {
+        Console.Write("F*ck you, ");
+        Console.WriteLine(username);
+    }
