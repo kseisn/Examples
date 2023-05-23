7e6f1bf62a21ae69a2a7b4b346a01bb5bcb24b8f
Kami Seisen <kamiseisen@gmail.com>
2023-05-23 18:23:14 +0200

    Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {13, 52, 38, 24, 59, 26, 71, 24, 68};
+int n = array.Length;
+int find = 24;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find) 
+    {
+        Console.Write("index = ");
+        Console.WriteLine(index);
+        break; // прерывает цикл
+    }
+    index++;
+}
